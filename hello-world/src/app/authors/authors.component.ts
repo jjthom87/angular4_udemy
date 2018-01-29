f9c40aed45912d2a160578ebb9ad2ad06a3fdeb3
Jared Thomas <jjthom87@yahoo.com>
2018-01-28 21:43:57 -0500

    adding

--- hello-world/src/app/authors/authors.component.ts
+++ hello-world/src/app/authors/authors.component.ts
@@ -11,8 +11,34 @@ export class AuthorsComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSave($event){
+    console.log($event);
+    $event.stopPropagation();
+    this.isActive = true;
+  }
+
+  onKeyUp($event){
+    var str = "";
+    str += $event.target.value
+    this.keyUpText = str;
+  }
+
+  onKeyUpEnter($event){
+    this.keyUpEnterText = $event.target.value
+  }
+
+  onEnterThis(whatever){
+    this.enterThisText = whatever
+  }
+
+  enterThisText = undefined
+  keyUpEnterText = undefined
+  keyUpText = ''
+  isActive = false;
+  colSpan = 2
   title = "3 Authors"
   authors;
+  image_url="https://www.thenation.com/wp-content/uploads/2017/09/vonnegut-SevenStoriesPress_img.jpg"
 
 	constructor(service: AuthorsService){
 		this.authors = service.getAuthors();
